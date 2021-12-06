94c97c6b90271e44fe3eb659b9e215b6c0f8d061
Minh Tam <htam0110@gmail.com>
2021-12-06 22:38:51 +0700

    update form giao vu

--- src/main/resources/static/asset/DATN/assets/js/roles/PHHS.js
+++ src/main/resources/static/asset/DATN/assets/js/roles/PHHS.js
@@ -5,82 +5,82 @@ var API_Approve = "http://localhost:8081/api/vi/Approve";
 
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
-  $routeProvider
-    // giao vien
-    .when("/thongtin", {
-      templateUrl: "./assets/views/phuhuynh/thongtin.html",
-    })
-    .when("/diem", {
-      templateUrl: "./assets/views/phuhuynh/diem.html",
-    })
-    .when("/pheduyet", {
-      templateUrl: "./assets/views/phuhuynh/guiphanhoi.html",
-    });
+    $routeProvider
+        // giao vien
+        .when("/", {
+            templateUrl: "./assets/views/phuhuynh/thongtin.html",
+        })
+        .when("/diem", {
+            templateUrl: "./assets/views/phuhuynh/diem.html",
+        })
+        .when("/pheduyet", {
+            templateUrl: "./assets/views/phuhuynh/guiphanhoi.html",
+        });
 });
 
 app.controller("try-Ctrl", function ($scope, $http) {
-  // biến lấy cookie theo cặp {name, value}
-  var cookies = document.cookie
-    .split(";")
-    .map((cookie) => cookie.split("="))
-    .reduce(
-      (accumulator, [key, value]) => ({
-        ...accumulator,
-        [key.trim()]: decodeURIComponent(value),
-      }),
-      {}
-    );
+    // biến lấy cookie theo cặp {name, value}
+    var cookies = document.cookie
+        .split(";")
+        .map((cookie) => cookie.split("="))
+        .reduce(
+            (accumulator, [key, value]) => ({
+                ...accumulator,
+                [key.trim()]: decodeURIComponent(value),
+            }),
+            {}
+        );
 
-  $scope.Approves = [];
-  $scope.form = {};
-  $scope.sdt = cookies.sdtphuhuynh;
-  //$scope.mahs = cookies.mahs;
-  //console.log($scope.mahs);
-
-  $scope.loading = function () {
-    // lấy sdt phụ huynh từ cookie
+    $scope.Approves = [];
+    $scope.form = {};
     $scope.sdt = cookies.sdtphuhuynh;
-    $http.get(`${API_Students}/SDTphuhuynh/${$scope.sdt}`).then((resp) => {
-      $scope.students = resp.data;
-      console.log($scope.students.data);
-    });
-  };
-  $scope.autoloading = function () {
-    if ($scope.sdt) {
-      $scope.loading();
-    } else {
-      window.location = "./Login_phuhuynh.html";
-    }
-  };
-  $scope.logout = function () {
-    if ($scope.sdt) {
-      window.localStorage.clear();
-      document.cookie = `sdtphuhuynh=`;
-      window.location = "./Login_phuhuynh.html";
-    }
-  };
+    //$scope.mahs = cookies.mahs;
+    //console.log($scope.mahs);
 
-  $scope.guiphanhoi = function () {
-    var Approve = angular.copy($scope.form);
-    console.log(Object.keys(Approve).length)
-    if (Object.keys(Approve).length != 0 ) {
-      $http
-        .post(`${API_Approve}/insert`, Approve)
-        .then((resp) => {
-          $scope.Approves.push(resp.data);
-          $scope.loading();
-          $scope.form = {};
-          alert("Gửi thành công");
-        })
-        .catch((error) => {
-          alert("Lỗi");
-          console.log("Error", error);
+    $scope.loading = function () {
+        // lấy sdt phụ huynh từ cookie
+        $scope.sdt = cookies.sdtphuhuynh;
+        $http.get(`${API_Students}/SDTphuhuynh/${$scope.sdt}`).then((resp) => {
+            $scope.students = resp.data;
+            console.log($scope.students.data);
         });
-    } else {
-      alert("Khong co dữ liệu");
-    }
-  };
+    };
+    $scope.autoloading = function () {
+        if ($scope.sdt) {
+            $scope.loading();
+        } else {
+            window.location = "./Login_phuhuynh.html";
+        }
+    };
+    $scope.logout = function () {
+        if ($scope.sdt) {
+            window.localStorage.clear();
+            document.cookie = `sdtphuhuynh=`;
+            window.location = "./Login_phuhuynh.html";
+        }
+    };
 
-  // khu vục chạy hàm
-  setInterval($scope.autoloading(), 300);
+    $scope.guiphanhoi = function () {
+        var Approve = angular.copy($scope.form);
+        console.log(Object.keys(Approve).length);
+        if (Object.keys(Approve).length != 0) {
+            $http
+                .post(`${API_Approve}/insert`, Approve)
+                .then((resp) => {
+                    $scope.Approves.push(resp.data);
+                    $scope.loading();
+                    $scope.form = {};
+                    alert("Gửi thành công");
+                })
+                .catch((error) => {
+                    alert("Lỗi");
+                    console.log("Error", error);
+                });
+        } else {
+            alert("Khong co dữ liệu");
+        }
+    };
+
+    // khu vục chạy hàm
+    setInterval($scope.autoloading(), 300);
 });
