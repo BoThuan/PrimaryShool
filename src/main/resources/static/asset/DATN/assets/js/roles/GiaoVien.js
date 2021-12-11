1d4d3d17331390d4b82bd8f9ed76f443708de421
ThuanNguyen <thhuanntv013@gmail.com>
2021-12-11 11:35:25 +0700

    liên kết giao diện

--- src/main/resources/static/asset/DATN/assets/js/roles/GiaoVien.js
+++ src/main/resources/static/asset/DATN/assets/js/roles/GiaoVien.js
@@ -1,4 +1,11 @@
 var app = angular.module("myApp", ["ngRoute"]);
+app.run(function ($http, $rootScope, $timeout) {
+    $rootScope.maGiaoVien = getCookie("maGiaoVien");
+    $rootScope.TenGiaoVien = getCookie("TenGiaoVien");
+    $rootScope.emailGiaoVien = getCookie("emailGiaoVien");
+    $rootScope.sdtGiaoVien = getCookie("sdtGiaoVien");
+  });
+
 app.config(function ($routeProvider) {
     $routeProvider
         // giao vien
@@ -23,3 +30,30 @@ app.config(function ($routeProvider) {
         })
         ;
 });
+
+const getCookie = (cookie_name) => {
+    // Construct a RegExp object as to include the variable name
+    const re = new RegExp(`(?<=${cookie_name}=)[^;]*`);
+    try {
+      return document.cookie.match(re)[0]; // Will raise TypeError if cookie is not found
+    } catch {
+      return "No Data";
+    }
+  };
+  
+  function logout() {
+    window.localStorage.clear();
+    deleteAllCookies();
+    window.location = "./login_nhansu.html";
+  }
+  
+  function deleteAllCookies() {
+      var cookies = document.cookie.split(";");
+  
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i];
+          var eqPos = cookie.indexOf("=");
+          var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+          document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      }
+  }
